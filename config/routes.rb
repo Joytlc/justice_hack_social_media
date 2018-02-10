aa3e56496a495da1a022e460658b3b48677b308d
John Doe <johndoe@example.com>
2018-02-10 13:27:47 -0500

    added bootstrap and carriew wave

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+  # get 'landing/home'
+  root 'landing#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
